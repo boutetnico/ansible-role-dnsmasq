71f233e87e2ba7b408e88dad7394ac538b58621f
Nicolas Boutet <amd3002@gmail.com>
2023-04-26 14:39:47 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,21 +5,28 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('name', [
-  ('dnsmasq'),
-])
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("dnsmasq"),
+    ],
+)
 def test_packages_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('username,groupname,path', [
-  ('root', 'root', '/etc/dnsmasq.d/cache'),
-  ('root', 'root', '/etc/dnsmasq.d/main'),
-])
+@pytest.mark.parametrize(
+    "username,groupname,path",
+    [
+        ("root", "root", "/etc/dnsmasq.d/cache"),
+        ("root", "root", "/etc/dnsmasq.d/main"),
+    ],
+)
 def test_config_files(host, username, groupname, path):
     config = host.file(path)
     assert config.exists
